a74593362a76c2e0fde04b4f37ce3bb214f866ba
nmayen <nmayen@imsa.com.gt>
2023-10-12 16:34:11 -0600

    Cambios: se crearon las diferentes modales del maestro de actividades

--- sitScrum/Controllers/ActividadesController.cs
+++ sitScrum/Controllers/ActividadesController.cs
@@ -1,4 +1,5 @@
 ﻿using clbBusinessInterface;
+using clbModels;
 using clbModels.Bitacora;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -17,18 +18,7 @@ namespace sitScrum.Controllers
         public IActionResult Index()
         {
 
-            // return View();
-
-            try
-            {
-                var Result = _IActividadesBO.Get(new clbModels.Bitacora.Scrum_Actividades());
-
-                return View(Result);
-            }
-            catch (Exception ex)
-            {
-                return BadRequest(_IActividadesBO.ErrorMessage + " => " + ex.Message);
-            }
+            return View();
 
         }
 
@@ -64,6 +54,55 @@ namespace sitScrum.Controllers
             catch (Exception ex)
             {
                 return BadRequest(_IActividadesBO.ErrorMessage + " => " + ex.Message);
+            }
+        }
+
+
+        [HttpPost]
+        public IActionResult ModificarRegistro(Scrum_Actividades model)
+        {
+            try
+            {
+                ResponseDB Result;
+
+                if (model.Accion.Equals("I"))
+                {
+                    Result = _IActividadesBO.Set(model).FirstOrDefault();
+                }
+                else
+                {
+                    Result = _IActividadesBO.Upd(model).FirstOrDefault();
+                }
+
+                if (Result != null)
+                {
+                    if (Result.existe_error.Equals("S"))
+                    {
+                        return BadRequest(_IActividadesBO.ErrorMessage + " => " + Result.mensaje);
+                    }
+                }
+
+                var Bolet = _IActividadesBO.Get(model).FirstOrDefault();
+                return PartialView("Row", Bolet);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(_IActividadesBO.ErrorMessage + " => " + ex.Message);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult EliminarRegistro(Scrum_Actividades model)
+        {
+            try
+            {
+                var vResponse = _IActividadesBO.Del(model).First();
+
+                return Json(new { Mensaje = "Proceso Ejecutado", Error = "N", Cod_Transaccion = vResponse.cod_error });
+            }
+            catch (Exception ex)
+            {
+                return Json(new { Mensaje = $"Ocurrio un Problema {ex.Message}", Error = "S" });
             }
         }
 
